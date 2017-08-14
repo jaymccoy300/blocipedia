10b36d7683d3d35db96ecf2ef562056e186dbab0
Jay McCoy <jaymccoy300@gmail.com>
2017-08-13 21:13:09 -0700

    Checkpoint-3 Complete

--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -3,17 +3,16 @@ require 'rails_helper'
 RSpec.describe WelcomeController, type: :controller do
 
   describe "GET #index" do
-    it "returns http success" do
+    it "returns http index success" do
       get :index
       expect(response).to have_http_status(:success)
     end
   end
 
   describe "GET #about" do
-    it "returns http success" do
+    it "returns http about success" do
       get :about
       expect(response).to have_http_status(:success)
     end
   end
-
 end
