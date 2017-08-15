8ba3de8dfaf2de0120caa7f00533c121edfcaef3
Jay McCoy <jaymccoy300@gmail.com>
2017-08-14 23:06:16 -0700

    Checkpoint-4 Complete

---
+++ app/controllers/wikis_controller.rb
@@ -0,0 +1,59 @@
+class WikisController < ApplicationController
+  def index
+    @wiki = Wiki.all
+  end
+
+  def show
+    @wiki = Wiki.find(params[:id])
+  end
+
+  def new
+    @wiki = Wiki.new
+  end
+
+  def edit
+    @wiki = Wiki.find(params[:id])
+  end
+
+  def create
+    @wiki = Wiki.new
+    @wiki.title = params[:wiki][:title]
+    @wiki.body = params[:wiki][:body]
+    @wiki.private = params[:wiki][:private]
+
+    if @wiki.save
+      flash[:notice] = 'Wiki was Saved!.'
+      redirect_to @wiki
+    else
+      flash.now[:alert] = 'There was an Error Saving Your Wiki. Please Try Again Later'
+      render :new
+    end
+  end
+
+  def update
+    @wiki = Wiki.find(params[:id])
+    @wiki.title = params[:wiki][:title]
+    @wiki.body = params[:wiki][:body]
+    @wiki.private = params[:wiki][:private]
+
+    if @wiki.save
+      flash[:notice] = 'Wiki Has Been Updated!.'
+      redirect_to @wiki
+    else
+      flash.now[:alert] = 'There was an Error Updating Your Wiki. Please Try Again Later'
+      render :edit
+    end
+  end
+
+  def destroy
+    @wiki = Wiki.find(params[:id])
+
+    if @wiki.destroy
+      flash[:notice] = "\"#{@wiki.title}\" was deleted successfully."
+      redirect_to wikis_path
+    else
+      flash.now[:alert] = "There was an error deleting the wiki."
+      render :show
+    end
+  end
+end
--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.describe WelcomeController, type: :controller do
 
+
   describe "GET #index" do
     it "returns http index success" do
       get :index
